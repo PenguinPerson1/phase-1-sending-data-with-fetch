22cfb1e1470705c3bae5dfeef6fef810fd680326
Annania Plossl <nia.plossl@gmail.com>
2024-03-15 11:35:49 -0400

    completed the lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Add your code here
+function submitData(name,email) {
+    const data = fetch("http://localhost:3000/users",{
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email,
+        }),
+    })
+    .then(results => results.json())
+    .then(message => document.querySelector("#text").textContent = message.id)
+    .catch(error => document.querySelector("#text").textContent = error)
+    return data;
+}
+
